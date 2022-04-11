0d69e1861ee59d3c8879306c85af0b334b7e5ed0
amanverma2253 <aman.v2253@gmail.com>
2022-04-11 21:04:39 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,78 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+
+const homeStartingContent = "Welcome to my blog page, here you can write and post your blogs. You can click on compose button and compose your ideas, article or anything else and publish it here.";
+const aboutContent = "I'm Aman Verma, B.E. sophomore at DSCE Banagalore. I'm a full stack (MERN) web-developer.";
+const contactContent = "Here are my contact details.";
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://amanv:Aman2253@cluster0.7dve6.mongodb.net/blogDB");
+
+let posts = [];
+
+app.get("/", function(req, res){
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+    });
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
+
+});
+
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port=3000;
+}
+app.listen(port, function(){
+  console.log("Server has started successfully");
+});
